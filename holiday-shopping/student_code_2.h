d7048d40ea430a5acec918f7026db1b61b33f294
Aziza Mirsaidova <57295556+azizamirsaidova@users.noreply.github.com>
2022-11-06 22:19:30 -0600

    Update student_code_2.h

--- holiday-shopping/student_code_2.h
+++ holiday-shopping/student_code_2.h
@@ -1,28 +1,9 @@
-///////////////////////////////////////////////
-// You need to
-//    1. Carefully read the instructions.
-//    2. Implement function MinCost.
-//    3. Compile your code.
-//    4. Run the executable on small and large
-//       unit tests.
-//    5. Test and debug your code. 
-//       Make sure that your program does not 
-//       have any memory leaks.
-//    6. Remove all commented out code. 
-//       Double check that your program does
-//       not print any debug information on 
-//       the screen.
-//    7. Submit your source code 
-//       ("student_code_2.h") on Canvas.
-///////////////////////////////////////////////
-
 //required libraries
 #include <string>
 #include <vector>
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-//you can include standard C++ libraries here
 
 struct Price
 {
