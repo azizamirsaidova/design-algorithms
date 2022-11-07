716f541e2b2fb4544585f613006ff4e560e85c9d
Aziza Mirsaidova <57295556+azizamirsaidova@users.noreply.github.com>
2022-11-06 22:19:05 -0600

    Update student_code_1.h

--- find-encryption-key/student_code_1.h
+++ find-encryption-key/student_code_1.h
@@ -1,21 +1,3 @@
-///////////////////////////////////////////////
-// You need to
-//    1. Carefully read the instructions.
-//    2. Implement function FindKey.
-//    3. Compile your code.
-//    4. Run the executable on small and large
-//       unit tests.
-//    5. Test and debug your code. 
-//       Make sure that your program does not 
-//       have any memory leaks.
-//    6. Remove all commented out code. 
-//       Double check that your program does
-//       not print any debug information on 
-//       the screen.
-//    7. Submit your source code 
-//       ("student_code_1.h") on Canvas.
-///////////////////////////////////////////////
-
 // required libraries
 #include <string>
 #include <vector>
@@ -74,7 +56,6 @@ std::string decrypt(const std::vector<int> &encryptedName, int key) {
 int FindKey(const std::vector<int> &encryptedName, int n,
             const std::string &restaurantList) 
 {
-  // write your code here
 
   std:: string temp = "";
   std:: unordered_set<std::string> subString;
