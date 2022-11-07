3db68845b6aa373fdfc4ef20fcebeb77b8e49470
Aziza Mirsaidova <57295556+azizamirsaidova@users.noreply.github.com>
2022-11-06 22:20:05 -0600

    Update student_code_4.h

--- maximum-flying-time/student_code_4.h
+++ maximum-flying-time/student_code_4.h
@@ -1,27 +1,8 @@
-///////////////////////////////////////////////
-// You need to
-//    1. Carefully read the instructions.
-//    2. Implement function MinCost.
-//    3. Compile your code.
-//    4. Run the executable on small and large
-//       unit tests.
-//    5. Test and debug your code. 
-//       Make sure that your program does not 
-//       have any memory leaks.
-//    6. Remove all commented out code. 
-//       Double check that your program does
-//       not print any debug information on 
-//       the screen.
-//    7. Submit your source code 
-//       ("student_code_4.h") on Canvas.
-///////////////////////////////////////////////
-
 //required libraries
 #include <string>
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-//you can include standard C++ libraries here
 
 std::string DefaultProblemSet()
 {
